f376d75e078abc3c54607b49a903b20004846be0
Andy Dang <andy.dang.it@gmail.com>
2021-02-09 22:26:27 -0600

    Add onclick in button

--- Design7/myScripts.js
+++ Design7/myScripts.js
@@ -1,4 +1,12 @@
 // This script manually  add text value
-document.getElementById("btn").addEventListener("click", function() {
+// document.getElementById("btn").addEventListener("click", function() {
+//     document.getElementById("msg").innerHTML = "hello world";
+// });
+
+function myFunction (){
+    var x = document.getElementById("myText").value;
+    document.getElementById("btn").addEventListener("click", function() {
     document.getElementById("msg").innerHTML = "hello world";
-});
+    });
+}
+
