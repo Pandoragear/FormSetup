3b7819d4672cc00e26e964833753087323fa0ea3
pandoragear <andy.dang.it@gmail.com>
2021-02-10 20:40:11 -0600

    Add JS code to btn

--- Design7/myScripts.js
+++ Design7/myScripts.js
@@ -3,10 +3,38 @@
 //     document.getElementById("msg").innerHTML = "hello world";
 // });
 
-function myFunction (){
-    var x = document.getElementById("myText").value;
-    document.getElementById("btn").addEventListener("click", function() {
-    document.getElementById("msg").innerHTML = "hello world";
-    });
+// function myFunction (){
+//     var x = document.getElementById("myText").value;
+//     document.getElementById("btn").addEventListener("click", function() {
+//     document.getElementById("msg").innerHTML = "hello world";
+//     });
+// }
+
+function createCloseButton(p) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("");
+
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  span.onclick = () => (span.parentElement.style.display = "none");
 }
 
+document.querySelectorAll("p").forEach(createCloseButton);
+
+document.querySelector("p").addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") e.target.classList.toggle("checked");
+});
+
+function add() {
+  let li = document.createElement("p");
+  let input_value = document.main.task.value;
+  let input_text = document.createTextNode(input_value);
+
+  li.appendChild(input_text);
+  document.querySelector("p").appendChild(li);
+  document.main.task.value = "";
+
+  createCloseButton(li);
+}
