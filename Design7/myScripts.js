6ec3748df3f5a6d0eeb93002f495873181ece9f3
pandoragear <andy.dang.it@gmail.com>
2021-02-10 21:52:55 -0600

    Added focus and comment to JS

--- Design7/myScripts.js
+++ Design7/myScripts.js
@@ -3,23 +3,23 @@
 //     document.getElementById("msg").innerHTML = "hello world";
 // });
 
-// function myFunction (){
-//     var x = document.getElementById("myText").value;
-//     document.getElementById("btn").addEventListener("click", function() {
-//     document.getElementById("msg").innerHTML = "hello world";
-//     });
-// }
 
 function showInput() {
 
+    // This create a condition if value is empty then show alert
   if (document.forms["main"].name.value === "") {
     alert("Please enter some text!!");
+
+    // This place the cursor back into the text field after alert
+    document.getElementById("name").focus();
   }
 
+  // This add message from text field onto the bottom line
   document.getElementById("msg").innerHTML = document.getElementById(
     "name"
   ).value;
 
+  // This clear the text field 
   document.getElementById("name").value = "";
 
 }
