2aae1e4609375dca81b562f539f54f37a0308498
pandoragear <andy.dang.it@gmail.com>
2021-02-10 21:16:25 -0600

    Modify JS submit button code

--- Design7/myScripts.js
+++ Design7/myScripts.js
@@ -10,44 +10,14 @@
 //     });
 // }
 
-function createCloseButton(p) {
-  let span = document.createElement("SPAN");
-  let txt = document.createTextNode("");
 
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+function showInput() {
+    document.getElementById('msg').innerHTML = document.getElementById("name").value;
 
-  span.onclick = () => (span.parentElement.style.display = "none");
-}
-
-document.querySelectorAll("p").forEach(createCloseButton);
-
-document.querySelector("p").addEventListener("click", (e) => {
-  if (e.target.tagName === "LI") e.target.classList.toggle("checked");
-});
-
-function add() {
-  let li = document.createElement("p");
-  let input_value = document.main.name.value;
-  let input_text = document.createTextNode(input_value);
-  
-  document.main.reset();
-  
-
-  li.appendChild(input_text);
-  document.querySelector("p").appendChild(li);
-  document.main.task.value = "";
-
-  createCloseButton(li);
-}
-
-
-// This function create an alert to show if value = Null 
-function add() {
-    if (document.forms['main'].name.value===""){
-        alert('empty');
+        if (document.forms['main'].name.value===""){
+        alert('Please enter a message!');
         return false;
     }
     return true;
+
 }
